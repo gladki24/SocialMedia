c02a59709b858beb02467b10c394fc4725228edd
Dawid Danel <dawid.danel@bluesoft.com>
2021-06-09 14:41:30 +0200

    fixes, parent link, h2 console

--- backend/src/main/java/com/socialmedia/config/security/SecurityConfig.java
+++ backend/src/main/java/com/socialmedia/config/security/SecurityConfig.java
@@ -48,7 +48,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     public void configure(WebSecurity web) {
         web.ignoring().antMatchers(
-                "h2-console/**",
+                "/h2-console/**",
                 "/v2/api-docs",
                 "/configuration/ui",
                 "/swagger-resources/**",
